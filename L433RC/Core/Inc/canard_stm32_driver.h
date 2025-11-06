e78eba057d17a3e5d2d3d883153f1d35ed16203d
SpiroJyro <sjinargyros@gmail.com>
2025-11-06 18:24:24 -0500

    Inital Bootloader L4333RC

---
+++ L433RC/Core/Inc/canard_stm32_driver.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "canard.h"
+/**
+  * @brief  Initialize HAL CAN and canard.
+  * @retval None
+  */
+void initCAN(void);
+
+/**
+  * @brief  Send all CAN Tx messages queued in canard.
+  * @retval None
+  */
+void sendCANTx(void);
+
+/**
+  * @brief  Execute periodic CAN maintenance tasks.
+  * @retval None
+  */
+void periodicCANTasks(void);
+
+void sendFirmwareRead(void);
+
+
+// FW update struct
+typedef struct {
+    char path[256];
+    uint8_t node_id;
+    uint8_t transfer_id;
+    uint32_t last_read_ms;
+    uint32_t offset;            // Byte offset in firmware file
+	uint32_t flash_address;     // Current staging flash write address
+	bool in_progress;           // Is update running?
+} FirmwareUpdate;
+
+extern FirmwareUpdate fwupdate;
